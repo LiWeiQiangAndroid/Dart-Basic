b9a40d66f56fe4b826eb2112bd6d47d552eddc91
liwq <lwqldsyzx@126.com>
2020-04-09 02:02:21 +0800

    Dart basic

---
+++ helloword.dart
@@ -0,0 +1,29 @@
+
+
+void main(){
+  print("hello world");
+  var a;
+  print(a);
+
+  a =  10;
+
+  print(a);
+
+  a = 'Hello World';
+
+  print(a);
+
+
+  var b = 20;
+
+  print(b);
+
+  final c = 30;
+
+
+  const d = 20;
+
+  print(d);
+
+
+}
